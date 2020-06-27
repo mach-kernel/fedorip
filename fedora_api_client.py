5f18cbf5138241c19e14e015d17c2a6865b9403b
David Stancu <dstancu@nyu.edu>
2020-06-27 15:40:39 -0400

    add client file

---
+++ fedora_api_client.py
@@ -0,0 +1,42 @@
+import json
+import logging
+
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+FR_FEDORA_API_URL = 'https://src.fedoraproject.org/api/0'
+
+log = logging.getLogger('client')
+log.setLevel(logging.DEBUG)
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+sh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+log.addHandler(sh)
+
+def fedora_search_pkgs(pattern, page=1, short=1):
+  query = {
+    'pattern': pattern,
+    'short': short,
+    'page': page,
+  }
+  url = '%s/projects?%s' % (FR_FEDORA_API_URL, urlencode(query))
+
+  try:
+    return json.loads(urlopen(url).read())
+  except HTTPError as e:
+    log.error('HTTP request to %s failed' % url)
+    log.error(e.read())
+
+def all_fedora_pkgs(pattern):
+  response = fedora_search_pkgs(pattern)
+  page = 1
+  max_page = response['pagination']['pages']
+
+  yield response
+
+  while page < max_page:
+    log.info('Fetching page %d of %d' % (page, max_page))
+    response = fedora_search_pkgs('*perl-*', page)
+    yield response
+    page += 1
